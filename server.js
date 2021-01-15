a643a0a1712a6535e756ea640da3b639ca54e001
aidan <aidanguarniere@gmail.com>
2021-01-14 17:11:34 -0800

    add post route

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//imports
+const inputCheck = require("./utils/inputCheck");
+
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -36,7 +39,7 @@ const db = new sqlite3.Database("./db/election.db", (err) => {
 
 // GET a single candidate
 // app.get('/api/candidate/:id', (req, res) => {
-//     const sql = `SELECT * FROM candidates 
+//     const sql = `SELECT * FROM candidates
 //                  WHERE id = ?`;
 //     const params = [req.params.id];
 //     db.get(sql, params, (err, row) => {
@@ -44,31 +47,60 @@ const db = new sqlite3.Database("./db/election.db", (err) => {
 //         res.status(400).json({ error: err.message });
 //         return;
 //       }
-  
+
 //       res.json({
 //         message: 'success',
 //         data: row
 //       });
 //     });
 //   });
-  
 
 // delete a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        return;
-      }
-  
-      res.json({
-        message: 'successfully deleted',
-        changes: this.changes
-      });
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: "successfully deleted",
+      changes: this.changes,
     });
   });
+});
+
+// create a candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ES5 function, not arrow function, to use `this`
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
 
 // default response for any other request(Not Found) Catch all
 app.use((req, res) => {
